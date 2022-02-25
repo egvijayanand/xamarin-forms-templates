065aa173690c18b13b8b0f7ee57ee6716d5443a9
Vijay Anand E G <81947404+egvijayanand@users.noreply.github.com>
2022-02-25 17:48:37 +0530

    Option to include CommunityToolkit NuGet package, ResourceDictionary item template and Branding icon

---
+++ src/FormsTemplatesCLI/FormsResDict/FormsResDict.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MyApp.Namespace
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class FormsResDict : ResourceDictionary
+    {
+        public FormsResDict()
+        {
+            InitializeComponent();
+        }
+    }
+}
